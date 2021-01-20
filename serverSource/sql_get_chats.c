f530a6136b0852ac3f0466a1c262e0fde5f3c4b6
Ilya Tsokota <iluha3211@gmail.com>
2021-01-20 17:47:24 +0200

    add doesn't work sending json file

--- serverSource/sql_get_chats.c
+++ serverSource/sql_get_chats.c
@@ -22,6 +22,8 @@ char *get_chats(MYSQL *con, char *user_id, int sock)
         finish_with_error(con);
     }
 
+    // free(bdrequest);
+
     MYSQL_RES *result = mysql_store_result(con);
 
     if (result == NULL)
@@ -34,48 +36,76 @@ char *get_chats(MYSQL *con, char *user_id, int sock)
     MYSQL_ROW row;
 
     chat_t *chat = NULL;
-    json_t *query = (json_t *)malloc(sizeof(json_t));
-    size_t  query_arr_length = 1024;
-    query->arr = (char**)malloc(sizeof(char*) * query_arr_length);
-    size_t length = 0;
 
-    const char* tmp_str;
+    const char *tmp_str;
+    char *file_name = strjoins(user_id, "_chats.json");
+    FILE *json = fopen(file_name, "a");
+    int size, read_size, stat, packet_index;
+    char send_buffer[10240], read_buffer[256];
+    fputs("{\"chats\": [", json);
     while ((row = mysql_fetch_row(result)))
     {
         chat = (chat_t *)malloc(sizeof(chat_t));
-        chat->ch_id = strdup(row[0]); //mx_strnew(strlen(row[0]));
-        chat->ch_name = strdup(row[1]);//mx_strnew(strlen(row[1]));
-        chat->ch_avatar = strdup(row[2]);//mx_strnew(strlen(row[2]));
-        chat->u_login = strdup(row[3]);//mx_strnew(strlen(row[3]));
-        chat->u_lastSeen = strdup(row[4]);//mx_strnew(strlen(row[4]));
-        chat->u_avatar = strdup(row[5]);//mx_strnew(strlen(row[5]));
+        chat->ch_id = strdup(row[0]);
+        chat->ch_name = strdup(row[1]);
+        chat->ch_avatar = strdup(row[2]);
+        chat->u_login = strdup(row[3]);
+        chat->u_lastSeen = strdup(row[4]);
+        chat->u_avatar = strdup(row[5]);
         tmp_str = write_chat_to_json(chat);
-        //puts(strdup(tmp_str));
-        query->arr[length] = strdup(tmp_str);
-        
-        free((void*)tmp_str);
-        free_chat_s(chat);
-        length++;
-        if  (length == query_arr_length) {
-            query_arr_length *= 2;
-            query->arr = (char**)realloc(query->arr, sizeof(char*) * query_arr_length);
+        //write to file
+        if (json != NULL)
+        {
+            fputs(tmp_str, json);
+            fputs(",", json);
         }
+        free((void *)tmp_str);
+        free_chat_s(chat);
     }
-    query->arr[length] = NULL;
+    fputs("]}", json);
     
-    char plug[3];
-    char *num = int_to_str(length);
-    write(sock, num, strlen(num));
-    free(num);
-    read(sock, plug, 2);
+    puts("GG1");
+    // find size file
+    fseek(json, 0, SEEK_END);
+    size = ftell(json);
+    fseek(json, 0, SEEK_SET);
    
-    for (size_t i = 0; i < length; i++)
+    puts("GG2");
+    //send size
+    puts(int_to_str(size));
+    write(sock, (void *)&size, sizeof(int));
+    puts("GG3");
+    do
     {
-        puts(query->arr[i]);
-        write(sock, query->arr[i], strlen(query->arr[i]));
-        read(sock, plug, 2);
+        stat = read(sock, &read_buffer, 255);
+    } while (stat < 0);
+    puts("GG4");
+    while (!feof(json))
+    {
+        read_size = fread(send_buffer, 1, sizeof(send_buffer) - 1, json);
+        do
+        {
+            stat = write(sock, send_buffer, read_size);
+        } while (stat < 0);
+        // packet_index++;
+        // bzero(send_buffer, sizeof(send_buffer));
     }
+    puts("GG5");
+    // char plug[3];
+    // char *num = int_to_str(length);
+    // write(sock, num, strlen(num));
+    // free(num);
+    // read(sock, plug, 2);
 
+    // for (size_t i = 0; i < length; i++)
+    // {
+    //     puts(query->arr[i]);
+    //     write(sock, query->arr[i], strlen(query->arr[i]));
+    //     read(sock, plug, 2);
+    // }
+     // fclose(json);
+    remove(file_name);
+    free(file_name);
     mysql_free_result(result);
     mysql_close(con);
     return "0"; //0 or >0
